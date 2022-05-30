bcd913d8fafa7c748c0ea4817ae1ded4eec4138e
Ray Yuan Liu <louis383@gmail.com>
2022-05-30 20:41:52 +0800

    git init

---
+++ app/src/main/java/com/rayliu/lifecycleexplorer/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.rayliu.lifecycleexplorer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import com.rayliu.lifecycleexplorer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+}
